ce344f90640b0921c43da81e034fb51374e59552
skyboy <luc@cacou.com>
2019-10-21 17:12:20 -0400

    Fin cour 16

---
+++ cours16/Assets/ImageShader/ScriptCamera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptCamera : MonoBehaviour {
+    private Material material;
+	// Use this for initialization
+	void Awake () {
+        material = new Material(Shader.Find("Hidden/NewImageEffectShader"));
+
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, material);
+
+    }
+}
